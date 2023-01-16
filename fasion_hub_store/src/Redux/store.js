e7453292574abc90f0fcdb754b14687b96117413
sumit_ss <sumitshashtrakar@gmail.com>
2023-01-16 23:33:06 +0530

    initial commit template with library

---
+++ fasion_hub_store/src/Redux/store.js
@@ -0,0 +1,9 @@
+
+
+import {  legacy_createStore } from "redux";
+// import thunk from "redux-thunk";
+// import {combineReducers} from "redux"
+
+// const rootReducer =combineReducers({})
+
+export const store =legacy_createStore()
---
+++ fasion_hub_store/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from './Redux/store';
+ import { ChakraProvider } from '@chakra-ui/react'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <ChakraProvider>
+      <Provider store={store}>
+       <BrowserRouter>
+         <App />
+        </BrowserRouter>
+    </Provider>
+ </ChakraProvider>
+  
+  
+);
+
+
