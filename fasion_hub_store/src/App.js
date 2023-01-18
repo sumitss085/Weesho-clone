3e610bdfc6550c854227275a14e1f2f5c53ab181
Nikhil Naidu <nikhilnaidu703@gmail.com>
2023-01-19 02:04:07 +0530

    done

--- fasion_hub_store/src/App.js
+++ fasion_hub_store/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
+import { WomenSaree } from './Component/Navbar/WomenSaree';
+import { AllRoutes } from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <AllRoutes/>
+
     </div>
   );
 }
--- fasion_hub_store/src/Redux/store.js
+++ fasion_hub_store/src/Redux/store.js
@@ -1,9 +1,10 @@
 
 
-import {  legacy_createStore } from "redux";
-// import thunk from "redux-thunk";
-// import {combineReducers} from "redux"
+import {  legacy_createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk"
+import { reducer } from "./Products/reducer";
 
-// const rootReducer =combineReducers({})
 
-export const store =legacy_createStore()
+const rootReducer =combineReducers({reducer})
+
+export const store =legacy_createStore(rootReducer, applyMiddleware(thunk))
