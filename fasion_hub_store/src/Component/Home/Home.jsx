1003a536e9d77bae36dcde232703fb3175fd6854
Vivek Maurya <vivekmauryarkt1998@gmail.com>
2023-01-20 21:11:57 +0530

    Added

--- fasion_hub_store/src/Component/Home/Home.jsx
+++ fasion_hub_store/src/Component/Home/Home.jsx
@@ -8,26 +8,33 @@ import {
     Box,
 } from '@chakra-ui/react'
 import styles from "./home.module.css";
+import Footer from "../Footer/Footer"
+import { Main } from './Styled-Home';
 export const Home = () => {
     return (
         <div>
-            <div className={styles.banner}>
-                <div className={styles.container}>
-                    <div className={styles.title}>
-                        Lowest Prices
-                        <div>Best Quality Shopping</div>
-                    </div>
-                    <div className={styles.title1}>
-                        50 lakh+ Styles | 650+ categories
-                    </div>
-                    <div className={styles.button}>
-                        <img alt="" width="8%" height="50%" src="https://www.freepnglogos.com/uploads/google-play-png-logo/google-severs-music-studio-png-logo-21.png" />
-                        <span>Download the Meesho App</span>
-                    </div>
-                </div>
-                <div className={styles.container1}>
-                </div>
-            </div>
+            <Main>
+            <section id="homepage">
+        <div>
+          <h1>Lowest Prices</h1>
+          <h1>Best Quality Shopping</h1>
+          <p>25+ Crores worth Rewards | 500 Winners every Hour</p>
+          <button>
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAcCAYAAAB75n/uAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJqSURBVHgBrZRPaxNBGMbfmd1oG+KfFBGpCBtBUPBgoB49BEzxpNSLx/YqXhJtTxqr/QLJfoKkX0ByEaQVDEIpAUtKBS2I3VgjWGLcEJq02Sb7OrNlY5pskk12H1hmdt6Z5zfz7B8yr6wt3vQUIyFvPq8TnAn4o3lwUWR+Zw1558apPxD2KgAIqSbF126BqNn5ql2A7OFlhoQ5AcmHgirPggui7TfZw8ljCICkI6Z2/yYURY3fAgeinQNtEAPETpPbLSWSDCSBGwALSCu2H6V4BNwAmJDN+qX2IYkQEuex/VLlB+AUwPXx4Irx8DskNRHTdmOjgyas1gJWEDO23E9VXgQnABNSaJyxKp1HxFc8NlV5MjsygOtt9RoUm17L2lhtT/KVv6UauXASc/ekkQB1FODN/vUuiEerwLnSltFHIHMN0JWj3HQrttavwq7O0jo89G2zVjPMJ/ayQPWjrnkEIM+aqO0TmKrop42TVLUm+IsbluZcbNcSu5JDA7iECsDFtG60/cROUR4a4N+vw+P3X2CiSEFYvw1QG+8xEzMC0NBQANPcX60f7/BgvAvCs2fXjCe4GiLBd3nbgDGtccK8Zcghn4IADQ/ftSyAFhSDK2mzLjoxb0Eqvoy4cjdKnkc3O2u2ANx8Uq1ZlcrMPkJiz5Z7rR0Y0aP17z3MUYYmDZDY0+V+68VB5lM7xU7jDFDBMo6hANNbhU5zlcUR7ReHlWgv8/DngnnLPkhMsDiuDorDSmIfczTiAFwisYUMjCixh7kKRF8iLxYS4FAUjVcN4M72b27+Pw4XzLlEgro8pZRe3t9QMk7jsNI/SVwmYkYExSAAAAAASUVORK5CYII="
+              alt=""
+            />
+            Download the <span> Meesho App</span>
+          </button>
+        </div>
+        <div>
+          <img
+            src="https://images.meesho.com/images/marketing/1631722939962.webp"
+            alt=""
+          />
+        </div>
+      </section>
+      </Main>
             <br />
             <div className={styles.line}>
                 <div className={styles.hr}></div>
@@ -153,6 +160,7 @@ export const Home = () => {
                             </AccordionButton>
                         </h2>
                         <AccordionPanel pb={4}>
+                            <Footer/>
                             <h1 style={{fontWeight:"bold" , textAlign:"left" }} >Meesho: Affordable Online Shopping at Your Fingertips</h1>
                             <p>There are many benefits of shopping online. You can take your time and look
                                  at different options to find exactly what you want. It's easy to compare prices 
