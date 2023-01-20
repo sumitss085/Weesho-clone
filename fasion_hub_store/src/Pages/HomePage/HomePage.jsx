e21d499da898f001b296eea1f83225816519b926
Vivek Maurya <vivekmauryarkt1998@gmail.com>
2023-01-20 18:35:55 +0530

    Added

--- fasion_hub_store/src/Pages/HomePage/HomePage.jsx
+++ fasion_hub_store/src/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Home } from '../../Component/Home/Home'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Home/>
+    </div>
   )
 }
 export default HomePage
