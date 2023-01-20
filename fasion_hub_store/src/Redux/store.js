36854d864ae5e6b9dcc585bc5fa581af99d71d38
Sumit_Shastrakar <107463250+sumitss085@users.noreply.github.com>
2023-01-20 07:17:24 +0530

    Update store.js

--- fasion_hub_store/src/Redux/store.js
+++ fasion_hub_store/src/Redux/store.js
@@ -4,28 +4,22 @@
 import {  applyMiddleware, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import {combineReducers} from "redux"
-
+import {sareeReducer} from "./WomensProducts/saree.reducer"
 import { AuthReducer } from "./AuthReducer/AuthReducer"; 
 
-const rootReducer =combineReducers({AuthReducer})
+const rootReducer =combineReducers({AuthReducer,sareeReducer})
 
 export const store =legacy_createStore(rootReducer,applyMiddleware (thunk))
 
-import {  legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk"
-import {sareeReducer} from "./WomensProducts/saree.reducer"
 
 
 
-const rootReducer =combineReducers({sareeReducer})
 
-export const store =legacy_createStore(rootReducer, applyMiddleware(thunk))
 
-//  import {  legacy_createStore } from "redux";
-// mport thunk from "redux-thunk";
-//  import {combineReducers} from "redux"
 
-//  const rootReducer =combineReducers({})
-//  export const store =legacy_createStore()
+
+
+
+
 
 
