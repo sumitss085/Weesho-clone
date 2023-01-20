3e497ef8533c0fa28e141d3159682d8aac945ffc
Shivamt2107 <shivam.gote2107@gmail.com>
2023-01-20 22:48:25 +0530

    add single product

--- fasion_hub_store/src/Component/SingleProductPage/SingleProductPage.jsx
+++ fasion_hub_store/src/Component/SingleProductPage/SingleProductPage.jsx
@@ -7,6 +7,7 @@ import { Progress, Stack } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+
 const SingleProductPage = () => {
 
   const  {  id } = useParams();
